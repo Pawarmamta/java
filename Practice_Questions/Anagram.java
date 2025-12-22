f866abea44dd9cd1d41a43b4158078529eae3e94
Mamta pawar <158306301+Pawarmamta@users.noreply.github.com>
2025-12-22 08:35:15 +0000

    O(nlogn)

---
+++ Practice_Questions/Anagram.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Anagram {
+    public static void main(String[] args) {
+        System.out.println("Enter two strings:");
+        Scanner sc = new Scanner(System.in);
+
+        String s1 = sc.nextLine().trim().toLowerCase();
+        String s2 = sc.nextLine().trim().toLowerCase();
+
+        if (s1.length() != s2.length()) {
+            System.out.println("Not Anagram");
+        } else {
+            char[] arr1 = s1.toCharArray();
+            char[] arr2 = s2.toCharArray();
+
+            Arrays.sort(arr1);
+            Arrays.sort(arr2);
+
+            if (Arrays.equals(arr1, arr2)) {
+                System.out.println("Anagram");
+            } else {
+                System.out.println("Not Anagram");
+            }
+        }
+    }
+}
